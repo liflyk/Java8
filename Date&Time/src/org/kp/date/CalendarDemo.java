32590d1deea07ff3ad7f401a90b3fc830a402f58
liflyk <liflyk@yahoo.com>
2019-10-20 15:34:50 +0530

    Adding demo Calendar

---
+++ Date&Time/src/org/kp/date/CalendarDemo.java
@@ -0,0 +1,14 @@
+package org.kp.date;
+
+import java.util.Calendar;
+
+public class CalendarDemo {
+
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+		
+		System.out.println("Current Date : " +cal.getTime());
+
+	}
+
+}
