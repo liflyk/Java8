d0e406e955851bad2519fafc0ea0e09164a94c19
liflyk <liflyk@yahoo.com>
2019-10-20 16:04:13 +0530

    modified date

--- Date&Time/src/org/kp/date/CalendarDemo.java
+++ Date&Time/src/org/kp/date/CalendarDemo.java
@@ -1,14 +1,24 @@
 package org.kp.date;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class CalendarDemo {
 
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
 		
+		cal.set(GregorianCalendar.YEAR, 2019);
+		
 		System.out.println("Current Date : " +cal.getTime());
 
+		//clear the calendar object
+		cal.clear();
+		
+		//add new calendar 
+		
+		cal.set(GregorianCalendar.YEAR, 2016);
+		System.out.println("Current Date : "+cal.getTime());
 	}
 
 }
